8ad9aeee792048054d92f63dc71122ee8d108402
Mustafa Jamal <mustafa52333@gmail.com>
2017-12-20 16:11:59 +0200

    Update biggest.py

--- week3/lecture/biggest.py
+++ week3/lecture/biggest.py
@@ -12,6 +12,9 @@ def biggest(aDict):
     """
     # it's one line, that's how I solved it ;)
     # update: This line works due to weakness of grade test case.
+    # now I've to note that this one line of code will return the highest alphabetical character 
+    # and the test case only have b with a list of some values and a with no values. 
+    # now this line of code don't look at the list at all. it just track the highest char and return it.
     return max(aDict.keys())
 
 # Exercise answer:
