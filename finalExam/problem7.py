5e77416495bfd2e53aec27bdf8389c15aa2f48aa
doct0r1 <mustafaj4m@gmail.com>
2018-02-07 00:14:24 +0200

    not-final yet

---
+++ finalExam/problem7.py
@@ -0,0 +1,18 @@
+class myDict(object):
+    """ Implements a dictionary without using a dictionary """
+
+    def __init__(self):
+        """ initialization of your representation """
+        # FILL THIS IN
+
+    def assign(self, k, v):
+        """ k (the key) and v (the value), immutable objects  """
+        # FILL THIS IN
+
+    def getval(self, k):
+        """ k, immutable object  """
+        # FILL THIS IN
+
+    def delete(self, k):
+        """ k, immutable object """
+        # FILL THIS IN
