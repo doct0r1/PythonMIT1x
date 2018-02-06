e5a66203845f28626beb4fcd82f97c9382fac749
doct0r1 <mustafaj4m@gmail.com>
2018-02-06 22:33:17 +0200

    problem5 w/ grade 14.55

--- finalExam/problem5.py
+++ finalExam/problem5.py
@@ -3,11 +3,25 @@ def uniqueValues(aDict):
     aDict: a dictionary
     returns: a sorted list of keys that map to unique aDict values, empty list if none
     """
-    global item
-    ls = list(aDict.values())
-    setLs = set(ls)
-    itemCounter = 0
+    try:
+        ls = list(aDict.values())
+    except AttributeError:
+        return []
+    returnedList = []
+    setLs = list(set(ls))
     for item in ls:
-        if item in ls:
-            itemCounter += 1
-    return list(setLs)
+        try:
+            if ls.count(item) > 1:
+                setLs.remove(item)
+        except ValueError:
+            continue
+    list(setLs)
+    for item in setLs:
+        returnedList.append(list(aDict.keys())[list(aDict.values()).index(item)])
+    return returnedList
+
+
+# print(uniqueValues({1: 1, 3: 2, 6: 0, 7: 0, 8: 4, 10: 0}))
+# print(uniqueValues({1: 1, 2: 1, 3: 1}))
+# print(uniqueValues({1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 7: 0, 8: 0, 9: 15}))
+# print(uniqueValues({0}))
