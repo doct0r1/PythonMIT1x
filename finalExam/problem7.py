8164f76b1f4bd9ddb50cc2f696773a78c4b8e356
doct0r1 <mustafaj4m@gmail.com>
2018-02-07 02:29:43 +0200

    p7 w/ 8/20 points

--- finalExam/problem7.py
+++ finalExam/problem7.py
@@ -1,18 +1,31 @@
 class myDict(object):
     """ Implements a dictionary without using a dictionary """
 
-    def __init__(self):
+    def __init__(self, ls=[]):
         """ initialization of your representation """
-        # FILL THIS IN
+        self.ls = ls
 
     def assign(self, k, v):
         """ k (the key) and v (the value), immutable objects  """
-        # FILL THIS IN
+        self.k = k
+        self.v = v
+        self.ls.extend((k, v))
 
     def getval(self, k):
         """ k, immutable object  """
-        # FILL THIS IN
+        self.k = k
+        # if k % 2 != 0:        # added those 2 lines after the assignment but I think they work
+        #     raise KeyError
+        if self.ls.count(k) > 1:
+            k += 1
+
+        return self.ls[self.ls.index(k) + 1]
 
     def delete(self, k):
         """ k, immutable object """
-        # FILL THIS IN
+        try:
+            self.k = k
+            del self.ls[self.ls.index(k) + 1]
+            self.ls.remove(k)
+        except:
+            raise KeyError
